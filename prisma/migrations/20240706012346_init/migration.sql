b3e79aeb4c8acda0acf3c7a4915c51a0798a1b9b
ejiro osiephri <osiephriejiro765@gmail.com>
2024-07-06 02:24:47 +0100

    Successfully connected prisma with postgresql

---
+++ prisma/migrations/20240706012346_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "userId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
