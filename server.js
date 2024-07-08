865e3aab37064c0254346adec397c58037c5dcc0
ejiro osiephri <osiephriejiro765@gmail.com>
2024-07-08 18:07:35 +0100

    Add: Added some minor changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const morgan = require("morgan");
 const userRoutes = require("./routes/UserRoutes");
 const checkUserRoutes = require("./routes/CheckUserRoutes");
 const organisationsRoute = require("./routes/OrganisationRoutes");
@@ -12,6 +13,10 @@ const PORT = process.env.NODE_ENV === "test" ? 4000 : process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan("combined"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", userRoutes);
 app.use("/api/users", checkUserRoutes);
